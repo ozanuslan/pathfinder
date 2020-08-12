02786b72b2a68204ececcd49e334c2052ab95b66
ozanuslan <ozanuslan@hotmail.com>
2020-08-12 18:14:52 +0300

    refactor

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,6 @@
 import PriorityQueue from './pq.js';
 
-const canvW = 802;
+const canvW = 1202;
 const canvH = 802;
 const rectW = 40;
 const rectH = 40;
@@ -8,14 +8,21 @@ var gridW = Math.floor(canvW / rectW);
 var gridH = Math.floor(canvH / rectH);
 var startX = 0;
 var startY = 0;
-var endX = 19;
+var endX = 29;
 var endY = 19;
 var grid = new Array(gridH);
 let unvisited = new PriorityQueue((a, b) => -a.dist > -b.dist);
+let frSlider;
+let fr = 60;
 
 function setup() {
+    frSlider = createSlider(1, 101, 51, 1);
+    frSlider.position(canvW / 2 - 120, canvH + 10);
+    frSlider.style('width', '240px');
     createCanvas(canvW, canvH);
     background(200);
+
+    // Creating the vertices
     for (var i = 0; i < gridH; i++) {
         grid[i] = new Array(gridW);
         for (var j = 0; j < gridW; j++) {
@@ -24,6 +31,8 @@ function setup() {
             unvisited.push(grid[i][j]);
         }
     }
+
+    // Creating the initial grid visual
     let clr;
     for (var i = 0; i < gridH; i++) {
         for (var j = 0; j < gridW; j++) {
@@ -34,48 +43,52 @@ function setup() {
             rect(1 + j * rectW, 1 + i * rectH, rectW, rectH);
         }
     }
-    console.log(unvisited);
-    console.log(grid);
 }
 
-async function draw() {
+function draw() {
+
+}
+
+function dijkstra() {
     let clr;
-    let curr;
-    if (!unvisited.isEmpty()) {
+    fr = frSlider.value();
+    frameRate(fr);
+    let curr = unvisited.peek();
+    if (!unvisited.isEmpty() && !(curr.x == endX && curr.y == endY)) {
         curr = unvisited.pop();
-        curr.state = false;
+        curr.inQueue = false;
         let neighbors = _neighbors(curr);
-        console.log(neighbors);
         let len = neighbors.length;
         let dist;
         for (var i = 0; i < len; i++) {
-            if(neighbors[i].x == curr.x || neighbors[i].y == curr.y) dist = 1; // If the vertex is not on a diagonal
-            else dist = 1.5; // If the vertex is diagonal
-            let alt = curr.dist + dist; 
+            if (neighbors[i].x == curr.x || neighbors[i].y == curr.y) dist = 1; // If the vertex is not on a diagonal
+            else dist = sqrt(2); // If the vertex is diagonal
+            let alt = curr.dist + dist;
             if (alt < neighbors[i].dist) {
                 neighbors[i].dist = alt;
                 neighbors[i].prev = curr;
             }
         }
+        var temp = new PriorityQueue((a, b) => -a.dist > -b.dist);
+        while (!unvisited.isEmpty()) {
+            temp.push(unvisited.pop());
+        }
+        unvisited = temp;
+
     }
-    var temp = new PriorityQueue((a, b) => -a.dist > -b.dist);
-    while (!unvisited.isEmpty()) {
-        temp.push(unvisited.pop());
-    }
-    unvisited = temp;
     for (var i = 0; i < gridH; i++) {
         for (var j = 0; j < gridW; j++) {
             if (grid[i][j].dist == 0) clr = [0, 255, 0];
             else if (i == endY && j == endX) clr = [255, 0, 0];
             else if (i == curr.y && j == curr.x) clr = [255, 0, 0];
-            else if (grid[i][j].state == false) clr = [0, 0, 255];
+            else if (grid[i][j].inQueue == false) clr = [0, 0, 255];
             else clr = [255];
             fill(clr);
             rect(1 + j * rectW, 1 + i * rectH, rectW, rectH);
         }
     }
-    if (unvisited.isEmpty()) {
-        fill(0,0,255);
+    if (curr.x == endX && curr.y == endY) {
+        fill(0, 0, 255);
         rect(1 + curr.x * rectW, 1 + curr.y * rectH, rectW, rectH);
         let path = new Array();
         let ver = grid[endY][endX];
@@ -87,16 +100,15 @@ async function draw() {
             fill(255, 255, 0);
             rect(1 + vertex.x * rectW, 1 + vertex.y * rectH, rectW, rectH);
         })
-        fill(0,255,0);
+        fill(0, 255, 0);
         rect(1 + startX * rectW, 1 + startY * rectH, rectW, rectH);
-        fill(255,0,0);
+        fill(255, 0, 0);
         rect(1 + endX * rectW, 1 + endY * rectH, rectW, rectH);
     }
-    
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    if (unvisited.isEmpty()) {
+        fill(0, 0, 255);
+        rect(1 + curr.x * rectW, 1 + curr.y * rectH, rectW, rectH);
+    }
 }
 
 function _neighbors(vertex) {
@@ -104,7 +116,7 @@ function _neighbors(vertex) {
     for (var i = 1; i >= -1; i--) {
         for (var j = 1; j >= -1; j--) {
             if (vertex.y + i >= 0 && vertex.y + i < gridH && vertex.x + j >= 0 && vertex.x + j < gridW) {
-                if (grid[vertex.y + i][vertex.x + j].state == true) neighbors.push(grid[vertex.y + i][vertex.x + j]);
+                if (grid[vertex.y + i][vertex.x + j].inQueue == true) neighbors.push(grid[vertex.y + i][vertex.x + j]);
             }
         }
     }
@@ -112,14 +124,15 @@ function _neighbors(vertex) {
 }
 
 window.setup = setup;
-window.draw = draw;
+window.draw = dijkstra;
 
 class Vertex {
     constructor(x, y, dist) {
         this.x = x;
         this.y = y;
         this.dist = dist;
-        this.state = true;
+        this.inQueue = true;
+        this.isWall = false;
         var prev;
     }
 }
